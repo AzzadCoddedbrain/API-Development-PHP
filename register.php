1c51c68ad5887200589ed64c8477dc4704339737
annusinha1 <annusinha15029@gmail.com>
2023-03-13 16:39:49 +0530

    email check in signup

--- register.php
+++ register.php
@@ -5,13 +5,22 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES (NULL,'$username','$email','$password')";
 
-$result= mysqli_query($conn, $sql) or die("Query Failed");
-if($result){
-    echo "done ";
-}else{
-    echo 'faild';
+$sql="select * from user where (email='$email')";
+$res=mysqli_query($conn,$sql);
+
+if (mysqli_num_rows($res)>0){
+    echo "user already exist";
 }
 
+else{
+        $sql = "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES (NULL, '$username', '$email', '$password')";
+
+        $result= mysqli_query($conn, $sql) or die("Query Failed");
+        if($result){
+            echo "done ";
+        }else{
+            echo 'faild';
+        }
+    }
 ?>
