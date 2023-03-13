f17d7e9322ca462e2f42915023342407561cdb63
annusinha1 <annusinha15029@gmail.com>
2023-03-13 17:38:23 +0530

    login get user details

--- login.php
+++ login.php
@@ -4,10 +4,14 @@ require 'connection.php';
 $email = $_POST['email'];
 $password = md5 ($_POST['password']);
 
-$sql="select * from user where (email='$email' and password ='$password')"; //check email & password
+$sql="select id, username,email from user where (email='$email' and password ='$password')"; //check email & password
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($result)>0){
+    
+    while($row = $result -> fetch_assoc()){ 
+        $response ['user']= $row;  //fetch email or password
+        }
     $response ['status_code'] = "200";
     $response ['message'] = "login sucessful";
 }
