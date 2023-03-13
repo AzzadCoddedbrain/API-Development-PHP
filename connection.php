0530586a996559e776356d19757bc21fd0bd16dc
AzzadCodeBrain <azzad@coddedbrain.com>
2023-03-13 14:42:32 +0530

    connection api

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$hostName = 'localhost';
+$userName = 'root';
+$userPass = '';
+$dbName = 'userdata';
+
+$conn = mysqli_connect($hostName,$userName,$userPass,$dbName);
+
+if(!$conn)
+{
+    echo"not connected";
+}else{
+    echo " connected";
+}
+
+
+?>
