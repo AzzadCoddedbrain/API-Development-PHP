ec91f17d18bee533a999978003e31c5e02325f68
annusinha1 <annusinha15029@gmail.com>
2023-03-14 16:18:12 +0530

    delete user account

--- deleteaccount.php
+++ deleteaccount.php
@@ -1,15 +1,29 @@
 <?php
 require 'connection.php';
-$userName = $_POST['username'];
-$delete = mysqli_query($conn, "DELETE FROM user WHERE username   = '$userName' ");
+$reqData = file_get_contents("php://input");
+$reqData = json_decode( $reqData );
+if( json_last_error() === JSON_ERROR_NONE ){
+    $conn->query("DELETE FROM user WHERE id = '{$reqData->id}' ");
 
-if($delete > 0){
-    $response ['status_code'] = "200";
-    $response ['message'] = "account has be deleted";
+    $deleted = (int) $conn->affected_rows;
+    if( $deleted ){
+        http_response_code( 200 );
+        header('Content-Type: Application/json; charset=utf-8');
+        $response ['status_code'] = "200";
+        $response ['message'] = "account has be deleted";
+    }
+    else{
+        http_response_code( 400 );
+        header('Content-Type: Application/json; charset=utf-8');
+        $response ['status_code'] = "400";
+        $response ['message'] = "account has not be deleted";
+    }
+    echo json_encode($response);
 }
+
 else{
-    $response ['status_code'] = "400";
-    $response ['message'] = "account has not be deleted";
-}
 
-echo json_encode($response);
+    echo json_encode([
+        "status" => false
+    ]);
+}
